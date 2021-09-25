a8a0d8a5e54a9e5dab14d6a6d11b0a2b0c1677eb
Maksongold <maxdudnik@mail.ru>
2021-09-25 17:12:51 +0300

    "Версия с ошибкой"

--- server/server.js
+++ server/server.js
@@ -1,40 +1,85 @@
-const http = require('http');
-const fs = require("fs");
-const pug = require("pug");
-const page404 = 'Error404';
-const mime = require("mime");
+'use strict';
 
-const server = http.createServer((req, res) => {
-    const url = req.url === '/' ? '../public/index.html' : req.url;
-    console.log(`${req.url}`);
-    if (url.endsWith(".pug")) {
-        const name = "template";
-        let templateFunc = pug.compileFileClient(
-            `../public/${url}`,{
-                name: name,
-                basedir: "../public/"
-            }) + "; export default  " +  name;
-        res.setHeader("Content-Type", "application/javascript");
-        res.write(templateFunc);
-        res.end();
-        return;
+const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid');
+const path = require('path');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(body.json());
+app.use(cookie());
+
+const users = {
+    'vasya@bk.ru': {
+        name: 'Vasya',
+        surname: 'Petrov',
+        email: 'vasya@bk.ru',
+        password: 'password',
+    },
+};
+const ids = {};
+
+// app.get('/', (req, res) => {
+//     const url = req.url === '/' ? '../public/index.html' : req.url;
+//     console.log(`${req.url}`);
+//     if (url.endsWith(".pug")) {
+//         const name = "template";
+//         let templateFunc = pug.compileFileClient(
+//             `../public/${url}`, {
+//                 name: name,
+//                 basedir: "../public/"
+//             }) + "; export default  " + name;
+//     }
+// });
+
+app.post('/signup', function (req, res) {
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const password = req.body.password;
+    const email = req.body.email;
+    if (
+        !password || !email ||
+        !password.match(/^\S{4,}$/) ||
+        !email.match(/@/)
+    ) {
+        return res.status(400).json({error: 'Не валидные данные пользователя'});
     }
-    fs.readFile(`../public/${url}`, (err, data) => {
-        if (req.url !== '/favicon.ico') {
-            if (err) {
-                data = page404;
-                res.write(data);
-                res.end();
-                return;
-            }
+    if (users[email]) {
+        return res.status(400).json({error: 'Пользователь уже существует'});
+    }
 
-            res.setHeader("Content-Type", mime.getType(url));
-            res.write(data);
-            res.end();
-        }
-        res.end();
-    });
+    const id = uuid();
+    const user = {name, surname, email, password};
+    ids[id] = email;
+    users[email] = user;
+
+    res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+    res.status(201).json({id});
 });
 
-console.log('listening at http://127.0.0.1:8081');
-server.listen(8081);
+app.post('/login', function (req, res) {
+    const password = req.body.password;
+    const email = req.body.email;
+    if (!password || !email) {
+        return res.status(400).json({error: 'Не указан E-Mail или пароль'});
+    }
+    if (!users[email] || users[email].password !== password) {
+        return res.status(400).json({error: 'Не верный E-Mail и/или пароль'});
+    }
+
+    const id = uuid();
+    ids[id] = email;
+
+    res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+    res.status(200).json({id});
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`Server listening port ${port}`);
+});

4adcad993fc96f704c5a5a299aaa55905e942a91
fillinmar <mar.nv17@yandex.ru>
2021-09-29 14:08:45 +0300

    clean ignore linter

--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -1,55 +1,55 @@
 (function() {
-    const AJAX_METHODS = {
-        POST: 'POST',
-        GET: 'GET',
-    };
+  const AJAX_METHODS = {
+    POST: 'POST',
+    GET: 'GET',
+  };
 
-    class Ajax {
-      getFetch(args = {}) {
-        let statusCode;
+  class Ajax {
+    getFetch(args = {}) {
+      let statusCode;
 
-        return fetch(args.url, {
-          method: AJAX_METHODS.GET,
-          credentials: 'include',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }).then((response) => {
-          statusCode = response.status;
-          return response.json();
-        }).then((parsedBody) => {
-          return {
-            status: statusCode,
-            parsedBody,
-          };
-        }).catch((parsedBody) => {
-        });
-      }
-
-      postFetch(args = {}) {
-        let statusCode;
-
-        return fetch(args.url, {
-          method: AJAX_METHODS.POST,
-          body: JSON.stringify(args.body),
-          credentials: 'include',
-          mode: 'cors',
-          headers: {
-            "Content-Type": "application/json",
-          }
-        }).then((response) => {
-          statusCode = response.status;
-          return response.json();
-        }).then((parsedBody) => {
-          return {
-            status: statusCode,
-            parsedBody
-          };
-        }).catch((response) => {
-          console.log(`error ${response.status}`);
-        })
-      }
+      return fetch(args.url, {
+        method: AJAX_METHODS.GET,
+        credentials: 'include',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((response) => {
+        statusCode = response.status;
+        return response.json();
+      }).then((parsedBody) => {
+        return {
+          status: statusCode,
+          parsedBody,
+        };
+      }).catch((parsedBody) => {
+      });
     }
-    window.Ajax = new Ajax();
+
+    postFetch(args = {}) {
+      let statusCode;
+
+      return fetch(args.url, {
+        method: AJAX_METHODS.POST,
+        body: JSON.stringify(args.body),
+        credentials: 'include',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((response) => {
+        statusCode = response.status;
+        return response.json();
+      }).then((parsedBody) => {
+        return {
+          status: statusCode,
+          parsedBody,
+        };
+      }).catch((response) => {
+        console.log(`error ${response.status}`);
+      });
+    }
+  }
+  window.Ajax = new Ajax();
 })();

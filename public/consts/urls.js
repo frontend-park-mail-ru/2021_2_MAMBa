32c24044d093969cb89819390cca739b880cc8cc
fillinmar <mar.nv17@yandex.ru>
2021-10-11 22:53:08 +0300

    MB-d74 Добавить локальный урл

--- public/consts/urls.js
+++ public/consts/urls.js
@@ -5,7 +5,7 @@ const deployUrl = 'https://film4u.club';
 const COLLECTIONS_COUNT = 12;
 const COLLECTIONS_LIMIT = 0;
 
-export const currentUrl = deployUrl;
+export const currentUrl = localUrl;
 
 export const URLS = {
   pages: {

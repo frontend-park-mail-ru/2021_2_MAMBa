53c8f3dc7c24784be834f4e22e4e5de5bd08f109
fillinmar <mar.nv17@yandex.ru>
2021-10-19 21:50:01 +0300

    MB-d74 почистить роутер

--- public/modules/router.js
+++ public/modules/router.js
@@ -30,14 +30,6 @@ class Router {
     eventBus.on(Events.RedirectBack, this.back.bind(this));
     eventBus.on(Events.RedirectForward, this.forward.bind(this));
 
-    // document.getElementById('root').addEventListener('click', (e) => {
-    //   console.log("in router");
-    //   if (e.target.dataset.routlink) {
-    //     e.preventDefault();
-    //     this.changeRoute(e.target.dataset.routlink);
-    //   }
-    // });
-
     this.application.addEventListener('click', (e) => {
       console.log("in router");
       const target = e.target;

375939f2d232c7a422bf710d0f8624866d6d9a1d
fillinmar <mar.nv17@yandex.ru>
2021-10-20 22:34:37 +0300

    MB-d74 Добавить BEM актеру

--- public/modules/adapters.js
+++ public/modules/adapters.js
@@ -11,58 +11,32 @@ export const arrayContentToActorPageContent = (actorInfoJson) => (
       gender: actorInfoJson.gender,
       family_status: actorInfoJson.family_status,
       film_number: actorInfoJson.film_number,
-
+      films: arrayContentFilms(actorInfoJson.film_list_with_actor),
+      films_with_description: arrayContentFilmsWithDescription(actorInfoJson.film_list_with_actor),
     }
 );
 
+export const arrayContentFilms = (arrayContent) => {
+      return arrayContent.reduce((arrayFilms, jsonFilm) => {
+            arrayFilms.push({
+                  id: jsonFilm?.id,
+                  title: jsonFilm?.title,
+                  film_avatar: `${jsonFilm?.picture_url}`,
+                  href: `/movie/${jsonFilm.id}`
+            });
+            return arrayFilms;
+      }, []);
+};
 
-// export const arrayContentToActorPageContent = (actorInfoJson) => (
-//     {
-//       // name: actorInfoJson.first_name + ' ' + actorInfoJson.last_name,
-//       // born: actorInfoJson.born,
-//       // actor_avatar: actorInfoJson.picture_url,
-//       // movies: arrayContentToNewFilmsSeries(actorInfoJson.movies),
-//     }
-// );
-// export const arrayContentToActorPageContent = (actorInfoJson) => (
-//     {
-//         name_rus: actorInfoJson.name_rus,
-//         // name_en: actorInfoJson.name_en,
-//         // career: actorInfoJson.career,
-//         // height: actorInfoJson.height,
-//         // born: actorInfoJson.born,
-//         // age: actorInfoJson.age,
-//         // birth_place: actorInfoJson.birth_place,
-//         // gender: actorInfoJson.gender,
-//         // family_status: actorInfoJson.family_status,
-//         // film_number: actorInfoJson.film_number,
-//         // actor_avatar: actorInfoJson.actor_avatar,
-//         // films: arrayContentFilms(actorInfoJson.film_list_with_actor),
-//         // films_with_description: arrayContentFilmsWithDescription(actorInfoJson.film_list_with_actor),
-//     }
-// );
-
-// export const arrayContentFilms = (arrayContent) => {
-//     return arrayContent.reduce((arrayFilms, jsonFilm) => {
-//         arrayFilms.push({
-//             id: jsonFilm?.id,
-//             title: jsonFilm?.title,
-//             film_avatar: `${jsonFilm?.picture_url}`,
-//             href: `/movie/${jsonFilm.id}`
-//         });
-//         return arrayFilms;
-//     }, []);
-// };
-//
-// export const arrayContentFilmsWithDescription = (arrayContent) => {
-//     return arrayContent.reduce((arrayFilmsWithDescription, jsonFilm) => {
-//         arrayFilmsWithDescription.push({
-//             id: jsonFilm?.id,
-//             title: jsonFilm?.title,
-//             film_avatar: `${jsonFilm?.picture_url}`,
-//             description:`${jsonFilm?.description}`,
-//             href: `/movie/${jsonFilm.id}`
-//         });
-//         return arrayFilmsWithDescription;
-//     }, []);
-// };
+export const arrayContentFilmsWithDescription = (arrayContent) => {
+    return arrayContent.reduce((arrayFilmsWithDescription, jsonFilm) => {
+        arrayFilmsWithDescription.push({
+            id: jsonFilm?.id,
+            title: jsonFilm?.title,
+            film_avatar: `${jsonFilm?.picture_url}`,
+            description:`${jsonFilm?.description}`,
+            href: `/movie/${jsonFilm.id}`
+        });
+        return arrayFilmsWithDescription;
+    }, []);
+};

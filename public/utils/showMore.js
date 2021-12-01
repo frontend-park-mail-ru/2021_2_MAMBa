ffb28210074d2e78fc2c41f394f794bdefd98775
fillinmar <mar.nv17@yandex.ru>
2021-12-01 16:53:49 +0300

    Добавить Бесконечный скролл на стр календаря

--- public/utils/showMore.js
+++ public/utils/showMore.js
@@ -25,7 +25,6 @@ export const showMore = (data, buttonClass, event) => {
 export const checkShowMoreButton = (available, buttonClass) => {
   const buttonShowMore = document.querySelector(`${buttonClass}`);
   if (!available && buttonShowMore) {
-    console.log(available);
     buttonShowMore.classList.add('hidden');
   }
 };
@@ -52,35 +51,4 @@ export const updateRenderFilmsData = (newData, dataOfRenderedFilms) => {
   dataOfRenderedFilms.moreAvailable = newData.moreAvailable;
   dataOfRenderedFilms.skip = newData.skip;
   dataOfRenderedFilms.limit = newData.limit;
-};
-
-
-export const showScrollMore = (data, event, condition) => {
-  if (!condition) {
-    condition = true;
-  }
-  const block = document.getElementById('infinite-scroll');
-  let contentHeight = block.offsetHeight;
-  window.addEventListener('scroll', () => {
-    const yOffset = window.pageYOffset;
-    const windowHeight = window.innerHeight;
-    const y = yOffset + windowHeight;
-    if (y >= contentHeight && condition) {
-      contentHeight = block.offsetHeight;
-      const newData = nextMonth(data.year, data.month);
-      data.year = newData[0];
-      data.month = newData[1];
-      eventBus.emit(event, data.year, data.month);
-    }
-  });
-
-  const nextMonth = (year, month) => {
-    if (month == 12) {
-      month = 1;
-      year += 1;
-    } else {
-      month += 1;
-    }
-    return [year, month];
-  };
 };

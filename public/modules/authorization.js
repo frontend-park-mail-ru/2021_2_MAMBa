db7a6c4485790178a1a6e73f014e277b6bd2418e
Maksongold <maxdudnik@mail.ru>
2021-12-05 20:37:38 +0300

    "Пофиксил мигающий навбар через локал стораж, убрал пуфто и исправил глупые плейсхолдеры в авторизации"

--- public/modules/authorization.js
+++ public/modules/authorization.js
@@ -22,6 +22,7 @@ class Authorization {
     }
     this.user = parsedResponse?.body;
     if (this.user) {
+      window.localStorage.setItem('user', JSON.stringify(this.user));
       this.eventBus.emit(EVENTS.authorization.gotUser);
       this.lastEvent = EVENTS.authorization.gotUser;
     }
@@ -33,6 +34,7 @@ class Authorization {
     }
     this.user = user;
     if (this.user) {
+      window.localStorage.setItem('user', JSON.stringify(this.user));
       this.eventBus.emit(EVENTS.authorization.changedUser);
     }
   }
@@ -76,6 +78,7 @@ class Authorization {
         this.eventBus.emit(EVENTS.App.ErrorPage);
       }
       if (response.status === statuses.OK) {
+        window.localStorage.removeItem('user');
         this.user = null;
         this.lastEvent = EVENTS.authorization.logOutUser;
       }
